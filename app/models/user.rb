68926599af8583d6384e75c9b1f1c3e8eb020054
Matsuphy <ma.ma28shi@gmail.com>
2024-11-07 18:36:32 +0900

    バリデーションの導入途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_k, presence: true
-  validates :first_name_k, presence: true
+  validates :last_name, presence: true,     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true,    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_k, presence: true,   format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_k, presence: true,  format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' }
   validates :date_of_birth, presence: true
 end
