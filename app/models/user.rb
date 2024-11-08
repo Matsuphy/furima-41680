626e0a25e111bc4447b9c0af3c6df2ead4a3025e
Matsuphy <ma.ma28shi@gmail.com>
2024-11-08 15:17:58 +0900

    userモデルに長音記号を含める

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true,     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates :first_name, presence: true,    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :last_name_k, presence: true,   format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' }
-  validates :first_name_k, presence: true,  format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_k, presence: true,   format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_k, presence: true,  format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
   validates :date_of_birth, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
