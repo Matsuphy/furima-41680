a16e8fbfacf263e88c0759f83716791ab528ee00
Matsuphy <ma.ma28shi@gmail.com>
2024-11-08 12:21:50 +0900

    gem fakerのインストール

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
+    email                 {'test@example.com'}
     password              {'ABC12345'}
     password_confirmation {password}
-    last_name             {}
-    first_name            {}
-    last_name_k           {}
-    first_name_k          {}
-    date_of_birth         {}
+    last_name             {'てすと'}
+    first_name            {'太郎'}
+    last_name_k           {'テスト'}
+    first_name_k          {'タロウ'}
+    date_of_birth         { Date.new(1990, 10, 12) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # ユーザー基本情報の入力
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     # ユーザー新規登録に関するテストコード
+    # ユーザー情報に関するテストコード
     it 'nicknameが空だと登録できない' do
-      user = FactoryBot.build(:user)
-      user.nickname = ''
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it '同じemailは登録できない' do
+      another_user = FactoryBot.build(:user)
+      another_user = @user.email
+      another_user.valid?
+      binding.pry
+      expect(another_user.errors.full_messages).to include
+    end
+    it 'emailは@を含まないと登録できない' do
+
+    end
+    it 'passwordが空では登録できない' do
+
+    end
+    it 'passwordは半角英数字混合の入力でなければ登録できない' do
+
+    end
+    it 'passwordとpassword_confirmationは同じでなければ登録できない' do
+
+    end
+    # ユーザー本人情報に関するテストコード
+    it '' do
+      
+    end
+    it '' do
+      
     end
     it '' do
       
