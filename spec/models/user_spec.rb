6324b413dd1ac113ff855b5941fb570cca90e1a6
Matsuphy <ma.ma28shi@gmail.com>
2024-11-08 11:46:54 +0900

    factorybotとrspec記載移動

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    # ユーザー新規登録に関するテストコード
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+  end
 end
