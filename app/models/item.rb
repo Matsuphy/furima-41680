ababf82aa1ed411b1cd7dfaf46aa8b285f665c6d
Matsuphy <ma.ma28shi@gmail.com>
2024-11-08 19:16:38 +0900

    activehashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -7,4 +8,11 @@ class Item < ApplicationRecord
     belongs_to_active_hash :shipping_fee
     belongs_to_active_hash :shipping_day
     belongs_to_active_hash :region
+
+
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :shipping_fee_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id, numericality: { other_than: 1 } 
+    validates :region_id, numericality: { other_than: 1 } 
 end
