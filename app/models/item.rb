0c29e3a240f0d205071dc4323fcb9e1a5dcdd028
Matsuphy <ma.ma28shi@gmail.com>
2024-11-08 19:12:58 +0900

    activehashのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :condition
+    belongs_to_active_hash :shipping_fee
+    belongs_to_active_hash :shipping_day
+    belongs_to_active_hash :region
 end
