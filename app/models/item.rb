c9fe3da761c342f9876db06a3ae3ce1ec8bf4d28
Matsuphy <ma.ma28shi@gmail.com>
2024-11-11 12:38:31 +0900

    itemカラムの単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image,             presence: true
-  validates :name,              presence: true, length: { maximum: 40, message: "40文字以内で入力してください" }
-  validates :item_description,  presence: true, length: { maximum: 1000, message: "1000文字以内で入力してください" }
+  validates :name,              presence: true, length:       { maximum: 40,      message: "40文字以内で入力してください" }
+  validates :item_description,  presence: true, length:       { maximum: 1000,    message: "1000文字以内で入力してください" }
   
-  validates :category_id,     numericality: { other_than: 1, message: "can't be blank" } 
-  validates :condition_id,    numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :region_id,       numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id,       presence: true, numericality: { only_integer: true, in: 2..11,        message: "is not valid"}
+  validates :condition_id,      presence: true, numericality: { only_integer: true, in: 2..7,         message: "is not valid" } 
+  validates :shipping_fee_id,   presence: true, numericality: { only_integer: true, in: 2..3,         message: "is not valid" } 
+  validates :region_id,         presence: true, numericality: { only_integer: true, in: 2..48,        message: "is not valid" } 
+  validates :shipping_day_id,   presence: true, numericality: { only_integer: true, in: 2..4,         message: "is not valid" } 
 
-  validates :price,             presence: true, numericality: {only_integer: true, in: 300..9999999, message: "¥300~¥9,999,999で入力してください"}
+  validates :price,             presence: true, numericality: { only_integer: true, in: 300..9999999, message: "¥300~¥9,999,999で入力してください"}
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {"テストテストテストテスト"}
+    item_description    {"テストテストテストテストテストテストテストテストテストテストテストテストテストテスト"}
+    category_id         {2}
+    condition_id        {3}
+    price               {30000}
+    shipping_fee_id     {3}
+    region_id           {20}
+    shipping_day_id     {2}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
