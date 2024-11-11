377c3c961b5000f7e242c6d0bccf8869fa6185eb
Matsuphy <ma.ma28shi@gmail.com>
2024-11-12 01:17:16 +0900

    ユーザーを指定するストロングパラメータ設定、編集画面の遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :move_to_index, only: [:edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -22,11 +23,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit( :name, :item_description, :category_id, :condition_id, 
                                   :price, :shipping_fee_id, :shipping_day_id, :region_id, :image,
                                   ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless user_signed_in? && current_user == item.user
+      redirect_to action: :index
+    end
   end
 
 end
