0d706008822bf18350db60ba98f6e1cbdff6f4ae
Matsuphy <ma.ma28shi@gmail.com>
2024-11-11 14:22:59 +0900

    itemモデルのuserアソシエーションのテストコードを記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -139,6 +139,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price ¥300~¥9,999,999で入力してください")
       end
+      # ユーザーアソシエーション
+      it 'userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("User must exist")
+      end
     end
   end
 end
