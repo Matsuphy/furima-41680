9f3dcc00a6674f9625f4d997ef02d3e7ced3db99
Matsuphy <ma.ma28shi@gmail.com>
2024-11-14 14:15:56 +0900

    with_optionsを使いpresence: trueを一括化

--- app/models/status_buyer.rb
+++ app/models/status_buyer.rb
@@ -2,17 +2,19 @@ class StatusBuyer
   include ActiveModel::Model
   attr_accessor :post_code, :region_id, :city, :street, :room, :phone_number, :user_id, :item_id, :token, :price
   # バリデーション
-  validates :post_code,         presence: true, format:       { with: /\A\d{3}[-]\d{4}\z/, message: "input correctly"}
-  validates :region_id,         presence: true, numericality: { only_integer: true, in: 2..48,        message: "is not valid" }
-  validates :city,              presence: true
-  validates :street,            presence: true
-  validates :phone_number,      presence: true, format:       { with: /\A\d{10,11}\z/, message: "input only 10 or 11 digits number"}
-
-  validates :token,             presence: true
-  validates :price,             presence: true, numericality: { only_integer: true, in: 300..9999999, message: "is not valid"}
-
-  validates :user_id,           presence: true
-  validates :item_id,           presence: true
+  with_options presence: true do
+    validates :post_code,         format:       { with: /\A\d{3}[-]\d{4}\z/, message: "input correctly"}
+    validates :region_id,         numericality: { only_integer: true, in: 2..48,        message: "is not valid" }
+    validates :city
+    validates :street
+    validates :phone_number,      format:       { with: /\A\d{10,11}\z/, message: "input only 10 or 11 digits number"}
+  
+    validates :token
+    validates :price,             numericality: { only_integer: true, in: 300..9999999, message: "is not valid"}
+  
+    validates :user_id
+    validates :item_id
+  end
   # 保存処理
   def save
     ActiveRecord::Base.transaction do
